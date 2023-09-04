596c61839cc86c0af1845e5dae25286970a046a8
Nebiat Mehari <nmehari25@gmail.com>
2023-09-04 13:19:10 +0100

    route using flask displays quote on local page

---
+++ initial_devops_exercise/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_quote():
+    return "I get DevOps!"
+
+if __name__ == '__main__':
+    app.run(
+    debug=True,
+#      port=5000,
+    host="0.0.0.0" # Listen for connections _to_ any server
+    )
